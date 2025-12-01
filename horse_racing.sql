c3c9aaaf2e253cc9cc3a8ef56a8532ce46316979
BushyCreate <104785194+BushyCreate@users.noreply.github.com>
2025-12-01 14:07:51 +0100

    Create horse_racing database and set context

--- horse_racing.sql
+++ horse_racing.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `horse_racing`
 --
-
+CREATE DATABASE `horse_racing`;
+USE `horse_racing`;
 -- --------------------------------------------------------
 
 --
